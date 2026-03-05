1b5a2e7dada54034f7ba3bf9b838bc8f0f4858c7
swaloure <orazayyeva@gmail.com>
2026-03-05 14:53:12 +0500

    Configure for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/heat1',
+  // basePath: '/heat1',
   typescript: {
     ignoreBuildErrors: true,
   },
